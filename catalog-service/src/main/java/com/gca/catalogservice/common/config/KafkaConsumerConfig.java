078aded1ed52bf62a883895b3596860b970d177d
Nhan Vu <nhan997@gmail.com>
2020-06-17 22:02:16 +0200

    config file properties

--- catalog-service/src/main/java/com/gca/catalogservice/common/config/KafkaConsumerConfig.java
+++ catalog-service/src/main/java/com/gca/catalogservice/common/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.gca.catalogservice.common.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -15,13 +16,15 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapServer;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                this.bootstrapServer);
         props.put(
                 ConsumerConfig.GROUP_ID_CONFIG,
                 "group_id_1");
--- catalog-service/src/main/java/com/gca/catalogservice/common/config/KafkaProducerConfig.java
+++ catalog-service/src/main/java/com/gca/catalogservice/common/config/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.gca.catalogservice.common.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,13 +14,15 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServer;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                this.bootstrapServer);
         configProps.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);

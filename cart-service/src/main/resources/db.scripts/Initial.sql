76b614732170ad167e0c89a62e4c000b7f9b1162
Jonas Goronczy <goronczy.jonas@gmail.com>
2020-06-30 01:02:27 +0200

    Removes cart init-script

--- cart-service/src/main/resources/db.scripts/Initial.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists webshop_cart.order
-(
-    id	bigserial not null constraint order_pk primary key,
-    session_id varchar not null,
-    product_id int not null,
-    quantity int not null
-);
